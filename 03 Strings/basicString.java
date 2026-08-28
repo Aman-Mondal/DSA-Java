5b4f3c48e05856c2ab7755a2693f4d78f63f21a7
amanalpha3389-star <amanalpha3389@gmail.com>
2026-08-28 19:06:06 +0530

    Update ReadMe

---
+++ 03 Strings/basicString.java
@@ -0,0 +1,9 @@
+public class basicString {
+    public static void main(String[] args) {
+
+        // 
+        char[] str = { 'A', 'l', 'p', 'h', 'a' };
+        String str2 = "Alpha";
+        String str3 = new String("Alpha");
+    }
+}
